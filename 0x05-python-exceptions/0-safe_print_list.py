0872d849f7008b04ee08fc1533a7eea370bd5a6e
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-11-20 09:02:03 -0800

    number zero

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    t = 0
+    take = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            t += 1
+            take += 1
         except IndexError:
             break
     print()
-    return(t)
+    return(take)
