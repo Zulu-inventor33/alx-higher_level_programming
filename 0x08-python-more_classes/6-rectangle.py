76c922f9b764fe1b52459f198c22a023096abcf7
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-12-02 02:59:28 -0800

    final number 6 commit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """this class will represent a rectangle"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a rectangle class
@@ -16,6 +17,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
