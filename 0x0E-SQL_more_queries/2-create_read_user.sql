023b7bc5566040ff14a13614efbc14edd5c520e4
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-01-17 18:51:43 -0800

    SQL questions commmit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- creates a database hbtn_0d_2 and a user user_0d_2.
+-- user_0d_2 should have only SELECT privilege in de database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
