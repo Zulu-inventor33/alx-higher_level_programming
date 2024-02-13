d065fe9e19f6a3e5bfd93617da458a05d77f8930
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-02-12 18:11:44 -0800

    checher now

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const myVar = ['C is fun','Python is cool','JavaScript is amazing']
-for(i = 0; i < myVar.length; i++)
-{
-	console.log(myVar[i]);
-}
+const myVar = ['C is fun','Python is cool','JavaScript is amazing'];
+myVar.forEach((Element) => {
+console.log(Element);
+})
