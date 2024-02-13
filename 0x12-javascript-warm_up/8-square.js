4087d494842ae589d2346928635f735154533f3d
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-02-12 18:52:14 -0800

    final commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 
-function add(a,b)
-{
-	console.log(a+b);
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
