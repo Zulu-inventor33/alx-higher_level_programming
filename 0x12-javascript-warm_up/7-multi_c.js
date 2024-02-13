78bdfefca2e67a1714add4e9369e448fc933365f
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-02-12 18:25:28 -0800

    correections

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
 
-for(i = 0; i < (parseInt(process.argv[2])); i++)
-	console.log('C is fun');
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
