963f096c9cc5ee2ad59a7fc302c2f55646f9285c
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-02-13 18:57:37 -0800

    objects in javascript done

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * A square class
+ * That inherits from rectangle class
+ */
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
