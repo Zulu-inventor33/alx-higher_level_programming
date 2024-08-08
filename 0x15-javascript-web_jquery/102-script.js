f56f42e9b8321e6f02c2283c1ae4f585d9860365
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-08-07 19:53:12 -0700

    commiting the jquery answers

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,12 @@
+//a JavaScript script that fetches and prints how to say “Hello” depending on the language
+$(document).ready(function () {
+	$("INPUT#btn_translate").click(function () {
+		const language_code = $("INPUT#language_code").val();
+		$.getJSON(
+			`https://fourtonfish.com/hellosalut/hello/?lang=${language_code}`,
+			function (data) {
+				$("#hello").text(data.hello);
+			}
+		);
+	});
+});
